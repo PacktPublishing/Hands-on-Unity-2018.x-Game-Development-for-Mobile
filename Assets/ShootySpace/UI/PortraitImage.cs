372adcbb6786a23c664cfe9a8867cc0ba47423e1
Ray Barrera <ray.barrera@aofl.com>
2018-08-18 10:49:39 -0700

    initial commit for Asset Bundles section

---
+++ Assets/ShootySpace/UI/PortraitImage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace MyCompany.ShootySpace.UI
+{
+	public class PortraitImage : MonoBehaviour
+	{
+		[SerializeField] private Image _portraitImage;
+	}
+}
